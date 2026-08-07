62e207d91943fb54b09544e7c9c3d344147be95f
kahjbot-stack <kahjbot@gmail.com>
2026-08-07 16:00:00 +0800

    script.js

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 function showPage(pageId) {
-var pages = document.querySelectorAll(’.page’);
+    var pages = document.querySelectorAll('.page');
 
-pages.forEach(function(page) {
-    page.classList.remove('active');
-});
-var target = document.getElementById(pageId);
-if (target) {
-    target.classList.add('active');
-}
-window.scrollTo(0, 0);
-var music = document.getElementById('bgMusic');
-if (music) {
-    music.play().catch(function() {});
-}
+    pages.forEach(function(page) {
+        page.classList.remove('active');
+    });
 
+    var target = document.getElementById(pageId);
+
+    if (target) {
+        target.classList.add('active');
+    }
+
+    window.scrollTo(0, 0);
+
+    var music = document.getElementById('bgMusic');
+
+    if (music) {
+        music.play().catch(function() {});
+    }
 }
