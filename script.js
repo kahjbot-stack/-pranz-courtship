f87591657e4ad4dcbe8245ec010907a5b968511d
kahjbot-stack <kahjbot@gmail.com>
2026-08-07 16:03:55 +0800

    script.js

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 function showPage(pageId) {
-    var pages = document.querySelectorAll('.page');
+    var pages = document.getElementsByClassName("page");
 
-    pages.forEach(function(page) {
-        page.classList.remove('active');
-    });
+    for (var i = 0; i < pages.length; i++) {
+        pages[i].classList.remove("active");
+    }
 
     var target = document.getElementById(pageId);
 
     if (target) {
-        target.classList.add('active');
+        target.classList.add("active");
     }
 
     window.scrollTo(0, 0);
-
-    var music = document.getElementById('bgMusic');
-
-    if (music) {
-        music.play().catch(function() {});
-    }
 }
