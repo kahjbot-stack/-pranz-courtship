737bd0e20871dd87367c1fdd00f9d4d9f61d40a9
kahjbot-stack <kahjbot@gmail.com>
2026-08-07 15:28:49 +0800

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 function showPage(pageId) {
-// Hide every page
 document.querySelectorAll(”.page”).forEach(page => {
 page.classList.remove(“active”);
 });
 
-// Show selected page
 const targetPage = document.getElementById(pageId);
 if (targetPage) {
     targetPage.classList.add("active");
 }
-// Scroll back to top
 window.scrollTo({
     top: 0,
     behavior: "smooth"
@@ -27,10 +24,10 @@ const musicButton = document.getElementById(“musicButton”);
 let musicPlaying = false;
 
 function toggleMusic() {
-
 if (!music) {
-    return;
+return;
 }
+
 if (musicPlaying) {
     music.pause();
     musicPlaying = false;
@@ -38,14 +35,16 @@ if (musicPlaying) {
         musicButton.innerHTML = "🎵";
     }
 } else {
-    music.play().then(() => {
-        musicPlaying = true;
-        if (musicButton) {
-            musicButton.innerHTML = "🔊";
-        }
-    }).catch(() => {
-        console.log("Music could not start.");
-    });
+    music.play()
+        .then(() => {
+            musicPlaying = true;
+            if (musicButton) {
+                musicButton.innerHTML = "🔊";
+            }
+        })
+        .catch(() => {
+            console.log("Music could not start.");
+        });
 }
 
 }
